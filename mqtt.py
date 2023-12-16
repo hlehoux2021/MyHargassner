0311b9af7c445d388c690cc727fd8e9721b061f2
Hervé LEHOUX <herve@lehoux.net>
2023-12-16 23:30:06 +0100

    16/12 23:29

--- mqtt.py
+++ mqtt.py
@@ -40,50 +40,53 @@ class MqttInformer():
                                       entity=TextInfo(name="KT", state="0")),
                              lambda *_: None)
 
+    def _init_sensors(self):
+        """This method init the sensors"""
+        if '$KT' in self._dict:
+            logging.debug('$KT in dict --> set_text')
+            self._kt.set_text(self._dict['$KT'])
+        if 'HargaWebApp' in self._dict:
+            logging.debug('HargaWebApp in dict --> set_text')
+            self._web_app.set_text(self._dict['HargaWebApp'])
+
     def start(self):
         """This method runs the MqttInformer, waiting for message on _info_queue"""
         _stage: str = ''
         while True:
             try:
-                msg = self._info_queue.get(block=True, timeout=5)
+                msg = self._info_queue.get(block=True, timeout=10)
                 _str_parts = msg.split(':')
-                self._dict[_str_parts[0]] = _str_parts[1]
-                logging.debug('MqttInformer added %s:%s to dict', _str_parts[0], _str_parts[1])
-                if _str_parts[0] == 'BL_ADDR':
-                    _stage = 'name_ok'
-                if _str_parts[0] == '$setkomm':
-                    _stage = 'id_ok'
+                if _stage == 'device_info_ok':
+                    # we are in normal mode, after init of device_info ; we handle new or modified values
+                    if ((_str_parts[0] in self._dict) and (_str_parts[1] != self._dict[_str_parts[0]])) or (not _str_parts[0] in self._dict):
+                        # the value is new or has changed
+                        logging.debug('new value:[%s/%s]', _str_parts[0], _str_parts[1])
+                        self._dict[_str_parts[0]] = _str_parts[1]
+                        if _str_parts[0] == 'HargaWebApp':
+                            self._web_app.set_text(_str_parts[1])
+                        if _str_parts[0] == 'KT':
+                            self._kt.set_text(_str_parts[1])
+                    else:
+                        logging.debug('ignored [%s/%s]', _str_parts[0], _str_parts[1])
+                else:
+                    # device_info is not yes init
+                    self._dict[_str_parts[0]] = _str_parts[1]
+                    logging.debug('MqttInformer added %s:%s to dict', _str_parts[0], _str_parts[1])
+                    if ('BL_ADDR' in self._dict) and ('$setkomm' in self._dict):
+                        # we have all the info to init device_info
+                        logging.debug('MqttInformer device_info_ok')
+                        # Define the device. At least one of `identifiers` or `connections` must be supplied
+                        lstr= list()
+                        if '$slogin key' in self._dict:
+                            lstr.append(self._dict["$slogin key"])
+                        if '$SN' in self._dict:
+                            lstr.append(self._dict["$SN"])
+                        if '$setkomm' in self._dict:
+                            lstr.append(self._dict["$setkomm"])
+                        self.device_info = DeviceInfo(name=self._dict['BL_ADDR'], identifiers=lstr)
+                        _stage = 'device_info_ok'
+                        # now we init the already available sensors
+                        self._init_sensors()
             except Empty:
                 logging.debug("Queue is empty")
-            if _stage == 'id_ok':
-                # Define the device. At least one of `identifiers` or `connections` must be supplied
-                lstr= list()
-                if '$slogin key' in self._dict:
-                    lstr.append(self._dict["$slogin key"])
-                if '$SN' in self._dict:
-                    lstr.append(self._dict["$SN"])
-                if '$setkomm' in self._dict:
-                    lstr.append(self._dict["$setkomm"])
-                self.device_info = DeviceInfo(name=self._dict['BL_ADDR'],
-                                              identifiers=lstr)
-                _stage = 'device_info_ok'
-                # we need to init first value of sensors
-                if '$KT' in self._dict:
-                    logging.debug('$KT in dict --> set_text')
-                    self._kt.set_text(self._dict['$KT'])
-
-            if _stage == 'device_info_ok':
-                if ((_str_parts[0] in self._dict) and (_str_parts[1] != self._dict[_str_parts[0]])) or (not _str_parts[0] in self._dict):
-                    # the value is new or has changed
-                    logging.debug('new value:[%s/%s]', _str_parts[0], _str_parts[1])
-                    self._dict[_str_parts[0]] = _str_parts[1]
-                    if _str_parts[0] == 'HargaWebApp':
-                        self._web_app.set_text(_str_parts[1])
-                    if _str_parts[0] == '$KT':
-                        self._kt.set_text(_str_parts[1])
-                else:
-                    logging.debug('ignored [%s/%s]', _str_parts[0], _str_parts[1])
-                    logging.debug('in dict:%s', (_str_parts[0] in self._dict) )
-
-
             logging.debug('MqttInformer stage is now %s', _stage)
